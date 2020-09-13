2531054b01642bd79dbf65898f8d3af495f4f2b0
vananh <anhvv13@gmail.com>
2020-09-13 11:42:13 +0700

    Partially completed Banking Microservices

---
+++ MicroRabbit/MicroRabbit.Banking.Data/Context/BankingDbContext.cs
@@ -0,0 +1,30 @@
+﻿using MicroRabbit.Banking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace MicroRabbit.Banking.Data.Context
+{
+    public class BankingDbContext : DbContext
+    {
+        public BankingDbContext(DbContextOptions<BankingDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+    // xử lý khi tầng chưa dbcontext k cùng với tầng khởi tạo service provider 
+    // https://medium.com/oppr/net-core-using-entity-framework-core-in-a-separate-project-e8636f9dc9e5
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<BankingDbContext>
+    {
+        public BankingDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile(@Directory.GetCurrentDirectory() + "/../MicroRabbit.Banking.Api/appsettings.json").Build();
+            var builder = new DbContextOptionsBuilder<BankingDbContext>();
+            var connectionString = configuration.GetConnectionString("BankingDbConnection");
+            builder.UseSqlServer(connectionString);
+            return new BankingDbContext(builder.Options);
+        }
+    }
+}
