81702203773b785ac6cc4d8fc3b207dd53dda0eb
vananh <anhvv13@gmail.com>
2020-09-13 17:27:28 +0700

    Implemented Publish and SendCommand for Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit/MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,6 +1,9 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,13 +13,27 @@ namespace MicroRabbit.Banking.Application.Services
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepository;
-        public AccountService(IAccountRepository accountRepository)
+        private readonly IEventBus _bus;
+        public AccountService(IAccountRepository accountRepository, IEventBus bus)
         {
+            _bus = bus;
             _accountRepository = accountRepository;
         }
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepository.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                accountTransfer.FromAccount,
+                accountTransfer.ToAccount,
+                accountTransfer.TransferAmount
+            );
+            // use bus to send a command message
+            _bus.SendCommand(createTransferCommand);
+
         }
     }
 }
---
+++ MicroRabbit/MicroRabbit.Banking.Domain/CommandHandlers/TransferCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using MicroRabbit.Banking.Domain.Commands;
+using MicroRabbit.Banking.Domain.Events;
+using MicroRabbit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Domain.CommandHandlers
+{
+    // All handlers should implement IRequestHandler interface
+    // IRequestHandler depends on two parameters. First — request, second — response
+    // transfer is command that sent through the bus
+    public class TransferCommandHandler : IRequestHandler<CreateTransferCommand, bool>
+    {
+        private readonly IEventBus _bus;
+        public TransferCommandHandler(IEventBus bus)
+        {
+            _bus = bus;
+        }
+        public Task<bool> Handle(CreateTransferCommand request, CancellationToken cancellationToken)
+        {
+            // publish event to RabbitMQ
+            _bus.Publish(new TransferCreatedEvent(request.From, request.To, request.Amount));
+
+            return Task.FromResult(true);
+        }
+    }
+}
